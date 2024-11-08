8ed9f4196fc4c80aac98c9d3a8574f129d6d01ee
zinon-software <business.zinon@gmail.com>
2024-11-08 09:52:10 +0300

    add question section

--- lib/features/challenges/data/models/question_model.dart
+++ lib/features/challenges/data/models/question_model.dart
@@ -1,11 +1,35 @@
 class QuestionModel {
+  final String id;
+  final String section;
   final String question;
   final List<String> options;
   final String correctAnswer;
 
   QuestionModel({
+    required this.id,
+    required this.section,
     required this.question,
     required this.options,
     required this.correctAnswer,
   });
+
+  factory QuestionModel.fromJson(Map<String, dynamic> json) {
+    return QuestionModel(
+      id: json['id'],
+      section: json['section'],
+      question: json['question'],
+      options: List<String>.from(json['options']),
+      correctAnswer: json['correct_answer'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'section': section,
+      'question': question,
+      'options': options,
+      'correct_answer': correctAnswer,
+    };
+  }
 }
--- lib/features/challenges/data/models/session_model.dart
+++ lib/features/challenges/data/models/session_model.dart
@@ -2,6 +2,7 @@ class SessionModel {
   final String id;
   final PlayerModel player1;
   final PlayerModel player2;
+  final String section;
   final String? currentTurnPlayerId;
   final String currntQuestionId;
   final int questionCount;
@@ -14,6 +15,7 @@ class SessionModel {
     required this.id,
     required this.player1,
     required this.player2,
+    required this.section,
     required this.currentTurnPlayerId,
     required this.currntQuestionId,
     required this.questionCount,
@@ -28,14 +30,14 @@ class SessionModel {
       id: json['id'],
       player1: PlayerModel.fromJson(json['player1']),
       player2: PlayerModel.fromJson(json['player2']),
+      section: json['section'],
       currentTurnPlayerId: json['current_turn_player_id'],
       currntQuestionId: json['currnt_question_id'],
       questionCount: json['question_count'],
       currentQuestionNumber: json['current_question_number'],
       startedAt: DateTime.parse(json['started_at']),
-      endedAt: json['ended_at'] != null
-          ? DateTime.parse(json['ended_at'])
-          : null,
+      endedAt:
+          json['ended_at'] != null ? DateTime.parse(json['ended_at']) : null,
       duration: Duration(seconds: json['duration']),
     );
   }
@@ -45,6 +47,7 @@ class SessionModel {
       'id': id,
       'player1': player1.toJson(),
       'player2': player2.toJson(),
+      'section': section,
       'current_turn_player_id': currentTurnPlayerId,
       'currnt_question_id': currntQuestionId,
       'question_count': questionCount,
@@ -60,6 +63,7 @@ class SessionModel {
     String? id,
     PlayerModel? player1,
     PlayerModel? player2,
+    String? section,
     String? currentTurnPlayerId,
     String? currntQuestionId,
     int? questionCount,
@@ -72,10 +76,12 @@ class SessionModel {
       id: id ?? this.id,
       player1: player1 ?? this.player1,
       player2: player2 ?? this.player2,
+      section: section ?? this.section,
       currentTurnPlayerId: currentTurnPlayerId ?? this.currentTurnPlayerId,
       currntQuestionId: currntQuestionId ?? this.currntQuestionId,
       questionCount: questionCount ?? this.questionCount,
-      currentQuestionNumber: currentQuestionNumber ?? this.currentQuestionNumber,
+      currentQuestionNumber:
+          currentQuestionNumber ?? this.currentQuestionNumber,
       startedAt: startedAt ?? this.startedAt,
       endedAt: endedAt ?? this.endedAt,
       duration: duration ?? this.duration,
@@ -105,7 +111,7 @@ class PlayerModel {
 
   // copy with
   PlayerModel copyWith({String? userId, int? score}) {
-    return PlayerModel( 
+    return PlayerModel(
       userId: userId ?? this.userId,
       score: score ?? this.score,
     );
